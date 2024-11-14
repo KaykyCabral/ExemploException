7169858b98137103d639b6eeacb32cce181654f9
Kayky <kayky.rodriguesc03@gmail.com>
2024-11-13 20:24:13 -0400

    ExemploExcepition

---
+++ Conta.cs
@@ -0,0 +1,22 @@
+﻿public class Conta
+{
+    private double _saldo;
+
+    public void SetSAldo(double saldo)
+    {
+        try
+        {
+            if(saldo >= 0)
+            {
+                _saldo = saldo;
+            }
+            else
+            {
+                throw new Exception("O saldo não pode ser negativo!");
+            }  
+        }catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿try
+{
+    Conta c = new Conta();
+    c.SetSAldo(-60);
+}catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
